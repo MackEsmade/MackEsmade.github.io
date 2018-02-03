9eea3877ed1c540aae8ef5e63773e43a6b2a36ac
MackEsmade <mac.esmade@gmail.com>
2018-02-03 20:04:31 +0800

    new

---
+++ js/main-scroll.js
@@ -0,0 +1,16 @@
+    // SCROLLSPY
+    
+		$(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 500;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 100
+    });
+
